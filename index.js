1d3c6d02aa85eb752b5c390c46cfcf3b5444a4a0
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-30 17:50:46 -0400

    commit tree dd78f37acd5f79a7b0437364d35abf249ed405bb

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const thankYouMessages = [];
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        thankYouMessages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+};
+writeCards();
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i--);
+    };
+};
